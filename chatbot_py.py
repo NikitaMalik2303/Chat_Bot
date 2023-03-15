47d82f2f504929f20e70cdfbeec80449bf4fddbb
NikitaMalik2303 <nikitamalik2303@gmail.com>
2023-03-15 13:44:32 +0530

    added

--- chatbot_py.py
+++ chatbot_py.py
@@ -34,8 +34,8 @@ def predict_class(sentence):
   ERROR_THRESHOLD = 0.25
   results = [[i,r] for i,r in enumerate(res) if r> ERROR_THRESHOLD]
 
-  results.sort(key=lambda x:x[1],reverse=True)
-  return_list=[]
+  results.sort(key = lambda x:x[1], reverse=True)
+  return_list=[] 
   for r in results:
     return_list.append({'intent': classes[r[0]],'probability':str(r[1])})
   return return_list
